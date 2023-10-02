4b73135fae1f82701404681ec45b33ed1d4e7dab
Dylan Marin <dylanmarin@Dylans-MacBook-Air.local>
2023-10-02 14:32:13 -0400

    finished lab 2.2

--- src/index.js
+++ src/index.js
@@ -11,3 +11,22 @@ root.render(
         <App/>
     </React.StrictMode>
 );
+
+var functionScoped = 2;
+let blockScoped = 5;
+const constant1 = functionScoped - blockScoped;
+let numberArray1 = [1, 2, 3, 4, 5];
+let stringArray1 = ['string1', 'string2'];
+
+let variableArray1 = [
+    functionScoped, blockScoped,
+    constant1, numberArray1, stringArray1
+];
+
+console.log('Working with Arrays:');
+console.log(functionScoped);
+console.log(blockScoped);
+console.log(constant1);
+console.log(numberArray1);
+console.log(stringArray1);
+console.log(variableArray1);

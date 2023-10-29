c7c2082bc97d30a617f98cc96303c8209d45f12e
Dylan Marin <dylanmarin@Dylans-MacBook-Air.local>
2023-10-29 16:32:21 -0400

    dashboard 3.1 done

--- src/Kanbas/Dashboard/CourseCard.js
+++ src/Kanbas/Dashboard/CourseCard.js
@@ -1,25 +1,39 @@
 import {Link} from "react-router-dom";
 import "./index.css"
 
-function CourseCard({course, index}) {
+function CourseCard({course, index, setCourse, deleteFunction}) {
     const {_id, name, number, startDate, endDate} = course;
     const course_heading = name;
     const course_subtitle = name + " - " + number;
     const course_tertiary = _id + " " + endDate;
 
     return (
-        <Link
-            to={`/Kanbas/Courses/${_id}`}
-            key={index}>
-            <div className={"card"}>
-                <img className={"card-img-top"} src="/img/react.png" alt="Card image cap"></img>
-                <div className={"card-body"}>
-                    <h5 className={"card-title m-0"}>{course_heading}</h5>
-                    <p className={"card-text m-0"}>{course_subtitle}</p>
-                    <p className={"card-text m-0"}>{course_tertiary}</p>
+        <>
+            <Link
+                to={`/Kanbas/Courses/${_id}`}
+                key={index}>
+                <div className={"card"}>
+                    <img className={"card-img-top"} src="/img/react.png" alt="Card image cap"></img>
+                    <div className={"card-body"}>
+                        <h5 className={"card-title m-0"}>{course_heading}</h5>
+                        <p className={"card-text m-0"}>{course_subtitle}</p>
+                        <p className={"card-text m-0"}>{course_tertiary}</p>
+                        <button className={"btn btn-warning me-2 mt-2"}
+                                onClick={(event) => {
+                                    event.preventDefault()
+                                    setCourse(course)
+                                }
+                                }>Edit
+                        </button>
+                        <button className={"btn btn-danger mt-2"} onClick={(event) => {
+                            event.preventDefault();
+                            deleteFunction(course._id);
+                        }}>Delete
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </Link>)
+            </Link>
+        </>)
 }
 
 export default CourseCard;
--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -2,28 +2,80 @@ import db from "../Database";
 import CourseCard from "./CourseCard";
 import "./index.css"
 import "../index.css"
+import {useState} from "react";
 
 function Dashboard() {
-    const courses = db.courses;
+    const [courses, setCourses] = useState(db.courses);
+    const [course, setCourse] = useState({
+        name: "New Course", number: "New Number",
+        startDate: "2023-09-10", endDate: "2023-12-15",
+    });
+    const addNewCourse = () => {
+        setCourses([...courses,
+            {
+                ...course,
+                _id: new Date().getTime()
+            }]);
+    };
+
+    const deleteCourse = (courseId) => {
+        setCourses(courses.filter((course) => course._id !== courseId));
+    };
+
+    const updateCourse = () => {
+        setCourses(
+            courses.map((c) => {
+                if (c._id === course._id) {
+                    return course;
+                } else {
+                    return c;
+                }
+            })
+        );
+    };
+
     return (
         <div className={"wd-main-content-window"}>
             <div>
                 <h2>Dashboard</h2>
                 <hr/>
             </div>
-
             <div className="wd-published-courses-window">
                 <div>
                     <h2>Published Courses</h2>
                     <hr/>
                 </div>
 
-                <div className="d-flex flex-row flex-wrap">
-                    {courses.map((course, index) => (
-                        <CourseCard
-                            course={course}
-                            index={index}/>
-                    ))}
+                <div className={"row"}>
+                    <div className="d-flex flex-row col flex-wrap">
+                        {courses.map((course, index) => (
+                            <CourseCard
+                                course={course}
+                                index={index}
+                                setCourse={setCourse}
+                                deleteFunction={deleteCourse}/>
+                        ))}
+                    </div>
+                    <div className={"col-2"}>
+                        <h5>Course</h5>
+                        <input value={course.name} className="form-control"
+                               onChange={(e) => setCourse({...course, name: e.target.value})}/>
+                        <input value={course.number} className="form-control"
+                               onChange={(e) => setCourse({...course, number: e.target.value})}/>
+                        <input value={course.startDate} className="form-control" type="date"
+                               onChange={(e) => setCourse({...course, startDate: e.target.value})}/>
+                        <input value={course.endDate} className="form-control" type="date"
+                               onChange={(e) => setCourse({...course, endDate: e.target.value})}/>
+
+
+                        <button className={"btn btn-success"} onClick={addNewCourse}>
+                            Add
+                        </button>
+                        <button className={"btn btn-primary"} onClick={updateCourse}>
+                            Update
+                        </button>
+
+                    </div>
                 </div>
             </div>
         </div>

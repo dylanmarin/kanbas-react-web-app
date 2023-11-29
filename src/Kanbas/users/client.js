72799ebb51f90542d07724d9b34fae00433154ed
Dylan Marin <dylanjmmarin@gmail.com>
2023-11-28 21:35:57 -0500

    server api url

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_API = process.env.REACT_APP_BASE_API_URL;
+export const BASE_API = process.env.REACT_APP_BASE_API;
 export const USERS_API = `${BASE_API}/api/users`;
 
 const request = axios.create({
